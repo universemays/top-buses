ad1349e01888ba90b494bbc82e104d256c94c732
Puthnith <18649570+puthnith@users.noreply.github.com>
2022-04-01 11:42:53 +0200

    fix: add loading progress bar

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,8 +1,10 @@
+import NextNProgress from "nextjs-progressbar";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <NextNProgress color="#48bb78" />
       <Component {...pageProps} />
     </ChakraProvider>
   );
